2ee029ff015e20f7be9ce3fae1c0701802a7e82b
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-01-21 18:49:42 +0200

    Add files via upload

---
+++ Sum Matrix Elements.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Sum_Matrix_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsAndCols = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[,] matrix = new int[rowsAndCols[0], rowsAndCols[1]];
+            int sum = 0;
+            for (int row = 0; row < rowsAndCols[0]; row++)
+            {
+                int[] inputOfMatrix = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+                for (int col = 0; col < rowsAndCols[1]; col++)
+                {
+                    matrix[row, col] = inputOfMatrix[col];
+                    sum += matrix[row, col];
+                }
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sum);
+        }
+
+        public static void PrintMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void ReadingMatrixFromConsole(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] inputOfMatrix = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = inputOfMatrix[col];
+                }
+            }
+        }
+    }
+}
